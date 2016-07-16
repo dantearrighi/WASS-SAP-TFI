ccbfc22ba2c8cd3e1e67563d75aa8e64020f8ab9
dantearrighi <dantea@fibertel.com.ar>
2016-07-16 18:17:35 -0300

    listo
    lsfdasdf

--- WASS-SAPTFI/WASS-SAPTFI/Controllers/TramiteController.cs
+++ WASS-SAPTFI/WASS-SAPTFI/Controllers/TramiteController.cs
@@ -7,6 +7,9 @@ using System.Web;
 using System.Web.Mvc;
 using Modelos;
 using Datos;
+using WASS_SAPTFI.ViewModels.Tramite;
+using WASS_SAPTFI.ViewModels.Persona;
+
 
 namespace WASS_SAPTFI.Controllers
 {
@@ -37,11 +40,40 @@ namespace WASS_SAPTFI.Controllers
 
         //
         // GET: /Tramite/Create
-
+        // TramiteVM tiene los datos para crear un tramite: Lista de Personas, Tipos de Tramite
         public ActionResult Create()
         {
-            ViewBag.ListaTipos_Tramite = db.Tipos_Tramites.ToList();
-            return View();
+            TramiteVM model = new TramiteVM();
+
+            //TIPOS DE TRAMITE
+            model.Lista_Tipos_Tramite = db.Tipos_Tramites.ToList();
+
+         /*       var lista = db.Tipos_Tramites.Select(tt => new SelectListItem
+            {
+                Value = tt.Id.ToString(),
+                Text = tt.Descripcion
+            });
+
+                model.Lista_Tipos_Tramite = lista;    */
+
+
+            model.Lista_Personas = new List<PersonaVM>();
+
+            //PERSONAS PARA LA LISTA
+            foreach(Persona p in db.Personas)
+            {
+                PersonaVM pVM = new PersonaVM();
+                pVM.Id = p.Id;
+                pVM.NombreYapellido = p.NombreYapellido;
+                pVM.DNI = p.DNI;
+                pVM.Tipo_Persona = p.Tipo_Persona;
+                
+                model.Lista_Personas.Add(pVM);
+            }
+
+            //ESTADOS
+            model.Estados = db.Estados.ToList();
+            return PartialView("_AltaTramite",model);
         }
 
         //
@@ -114,6 +146,43 @@ namespace WASS_SAPTFI.Controllers
             db.Tramites.Remove(tramite);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+       public ActionResult ListarTramites()
+        {
+            //Armo la lista
+            List<ListaTramiteVM> listaTramites = new List<ListaTramiteVM>();
+            listaTramites.Clear();
+            
+           //Para cada uno de los tramites
+            foreach (Tramite itemLista in db.Tramites.ToList())
+            {
+                ListaTramiteVM ltvm = new ListaTramiteVM();
+
+            //Mapeo los datos entre los tramites y el TramiteViewModel
+                ltvm.Id = itemLista.Id;
+                ltvm.DNI = itemLista.Persona.DNI;
+                ltvm.NombreYapellido = itemLista.Persona.NombreYapellido;
+                itemLista.Detalles_Tramite.ToList();
+                //Obtengo la descripción correspondiente a la ultima fecha y la fecha
+                 foreach (Detalles_Tramite dt in itemLista.Detalles_Tramite.ToList())
+                 {
+                     //Si la fecha del detalle es la ultima, me quedo con el dato descripcion y fecha
+                     if (dt.Fecha_Desde == dt.Tramite.Detalles_Tramite.OrderByDescending(dddt => dddt.Fecha_Desde).FirstOrDefault().Fecha_Desde)
+                         //itemLista.Detalles_Tramite.ToList().OrderByDescending(ddt => ddt.Fecha_Desde).ToList().FirstOrDefault().Fecha_Desde)
+                     {
+                         ltvm.Ultimo_Detalle = dt.Descripcion;
+                         ltvm.Ultimo_Movimiento = dt.Fecha_Desde;
+                     }
+                 }
+
+                //Lo añado a la lista
+                listaTramites.Add(ltvm);
+
+            }
+
+            //Corro la vista parcial _ListaTramites
+            return PartialView("_ListaTramites", listaTramites);
         }
 
         protected override void Dispose(bool disposing)
